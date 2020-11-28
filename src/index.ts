97db5bc7050fcc840e0f2512563549c0780b0b0b
Brent Hamilton <hamilton.bh9@gmail.com>
2020-11-27 22:50:20 -0800

    removeOne, removeAll

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ export default class ModelBase<T> {
     return updated;
   }
 
-  async upsert(entity: Partial<T>) : Promise<T>{
+  async upsert(entity: Partial<T>) : Promise<T> {
     if (!this.upsertConflictKeys) {
       throw new Error('upsert conflict keys must be specified to use upsert function');
     }
@@ -57,6 +57,25 @@ export default class ModelBase<T> {
     const found = await this.fetchOne(where);
     if (found) return this.update(where, entity);
     return this.create(entity);
+  }
+
+  async removeOne(where: Partial<T>): Promise<T> {
+    const toRemove = await this.fetchAll(where);     
+    if (toRemove.length !== 1) {
+      throw new Error(`removeOne may only remove a single record. query returned ${toRemove.length}`);
+    }
+    await this.db(this.table)
+      .where(this.snakeKeys(where))
+      .del()
+    return toRemove[0];
+  }
+
+  async removeAll(where: Partial<T>): Promise<T[]> {
+    const toRemove = await this.fetchAll(where);     
+    await this.db(this.table)
+      .where(this.snakeKeys(where))
+      .del()
+    return toRemove;
   }
 }
 
