8872116da5d6504fbe21377970606551efa34ac0
Brent Hamilton <hamilton.bh9@gmail.com>
2022-04-30 01:53:56 -0700

    pass trx as part of config. include orderby options in fetchAll config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import {Knex} from 'knex';
 import { camelCase, snakeCase, first, pick } from 'lodash';
 
+type TConfig = {
+  trx: Knex,
+};
+
 export default class ModelBase<T> {
   constructor(
     protected db: Knex,
@@ -20,60 +24,78 @@ export default class ModelBase<T> {
     }, {} as T);
   }
 
-  async fetchAll(where?: Partial<T>, trx = this.db): Promise<T[]> {
-    let query = trx(this.table);
-    if (where) {
+  async fetchAll(
+    where?: Partial<T>,
+    config: TConfig & {
+      order?: {
+        [key: string]: 'asc' | 'desc',
+      },
+    } = {trx: this.db},
+  ): Promise<T[]> {
+    let query = config.trx(this.table);
+    if (where != null) {
       query = query.where(this.snakeKeys(where));
+    }
+    if (config.order != null) {
+      const orderBy = Object.keys(config.order).map((column) => ({
+        column,
+        order: config?.order?.[column],
+      }));
+      query = query.orderBy(orderBy);
     }
     return (await query).map(this.camelKeys);
   }
 
-  async fetchOne(where: Partial<T>, trx = this.db): Promise<T | undefined> {
+  async fetchOne(where: Partial<T>, config: TConfig): Promise<T | undefined> {
     // do we care about too many records?
     // this blindly returns the first one
-    return this.fetchAll(where, trx).then(first);
+    return this.fetchAll(where, config).then(first);
   }
 
-  async create(entity: Partial<T>, trx = this.db): Promise<T> {
-    await trx(this.table).insert(this.snakeKeys(entity));
+  async create(entity: Partial<T>, config: TConfig = {trx: this.db}): Promise<T> {
+    await config.trx(this.table).insert(this.snakeKeys(entity));
     return entity as T;
   }
 
-  async update(where: Partial<T>, what: Partial<T>, trx = this.db): Promise<T> {
-    await trx(this.table)
+  async update(
+    where: Partial<T>,
+    what: Partial<T>,
+    config: TConfig = {trx: this.db}
+  ): Promise<T> {
+    await config.trx(this.table)
       .update(this.snakeKeys(what))
       .where(this.snakeKeys(where));
-    const updated = await this.fetchOne(where, trx);
+    const updated = await this.fetchOne(where, config);
     if (!updated) {
       throw new Error('Entity cannot be null');
     }
     return updated;
   }
 
-  async upsert(entity: Partial<T>, trx = this.db) : Promise<T> {
+  async upsert(entity: Partial<T>, config: TConfig = {trx: this.db}) : Promise<T> {
     if (!this.upsertConflictKeys) {
       throw new Error('upsert conflict keys must be specified to use upsert function');
     }
     const where: Partial<T> = pick(entity, this.upsertConflictKeys);
-    const found = await this.fetchOne(where, trx);
-    if (found) return this.update(where, entity, trx);
-    return this.create(entity, trx);
+    const found = await this.fetchOne(where, config);
+    if (found) return this.update(where, entity, config);
+    return this.create(entity, config);
   }
 
-  async removeOne(where: Partial<T>, trx = this.db): Promise<T> {
-    const toRemove = await this.fetchAll(where, trx);
+  async removeOne(where: Partial<T>, config: TConfig = {trx: this.db}): Promise<T> {
+    const toRemove = await this.fetchAll(where, config);
     if (toRemove.length !== 1) {
       throw new Error(`removeOne may only remove a single record. query returned ${toRemove.length}`);
     }
-    await trx(this.table)
+    await config.trx(this.table)
       .where(this.snakeKeys(where))
       .del()
     return toRemove[0];
   }
 
-  async removeAll(where: Partial<T>, trx = this.db): Promise<T[]> {
-    const toRemove = await this.fetchAll(where, trx);     
-    await trx(this.table)
+  async removeAll(where: Partial<T>, config: TConfig = {trx: this.db}): Promise<T[]> {
+    const toRemove = await this.fetchAll(where, config);     
+    await config.trx(this.table)
       .where(this.snakeKeys(where))
       .del()
     return toRemove;
